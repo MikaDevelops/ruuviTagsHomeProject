42cbf5aa9149429194e65630f8e96ee445689573
mikavirtala <mika.virtala@gmail.com>
2024-11-16 15:08:45 +0200

    temporary script ruuvi names to db

---
+++ temp_script_macs_to_db.py
@@ -0,0 +1,24 @@
+from setup_ble_station.settings_load import settings_from_file
+from tags import macs
+from db.Sqlite_handler import Sqlite_handler
+
+settings = settings_from_file("./settings.json")
+macs_from_file: list = settings["ruuviTags"]["macs"]
+
+addition = ""
+macsLength = len(macs_from_file)
+i = 0
+for macF in macs_from_file:
+    name_of_ruuvi = macs[macF]
+    
+    addition += f"('{macF}', '{name_of_ruuvi}', 1)"
+    i+=1
+    if (i < macsLength):
+        addition +=", "
+sql_string: str = "INSERT INTO macs(mac, nimi, recorded) VALUES " + addition
+
+db = Sqlite_handler()
+conn = db.get_connection()
+cursor = conn.cursor()
+cursor.execute(sql_string)
+conn.commit()
